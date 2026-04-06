ccd9442bb226c1567f378d2536d4594c91b7168d
brian kipchirchir <brian.kipchirchir@caavagroup.com>
2026-04-06 15:39:07 +0300

    kk

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://fuliza-boost2.vercel.app/")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
             }
         };
